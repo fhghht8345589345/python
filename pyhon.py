7138f3434e98fd4c3d2e3f36c4261c6889ea4f4a
Aleksand golovanov <golovanovbiba@gmail.com>
2025-05-13 11:44:12 +0300

    print,imput

--- pyhon.py
+++ pyhon.py
@@ -1,1 +1,9 @@
-print("hello world")
+#comment
+# print("приз:",6,7,9,8,".", sep="*",end="?")
+#print("line \t joe \\")
+# print("репавн", 5//3)
+# print("репавн",min(5,6,8,9),round(5/5))
+
+input("напиши свой ник: ")
+
+
