804493d1effc53109b1dbecb3abc9506d3755474
Aleksand golovanov <golovanovbiba@gmail.com>
2025-05-13 12:37:51 +0300

    peremenad

---
+++ program.py
@@ -0,0 +1,16 @@
+box1=int(input("ведите код 1:"))
+
+box2=int(input("ведите код 2:"))
+
+box1 +=4
+
+
+print("Result",box1+box2)
+print("Result",box1-box2)
+print("Result",box1*box2)
+print("Result",box1**box2)
+print("Result",box1//box2)
+
+doi ="pr"
+doi=True
+print(doi*2)
