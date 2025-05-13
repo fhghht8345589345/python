b9a855bf429a86c49318afebca5d33219fd258c4
Aleksand golovanov <golovanovbiba@gmail.com>
2025-05-13 12:16:46 +0300

    peremana

--- pyhon.py
+++ pyhon.py
@@ -3,7 +3,16 @@
 #print("line \t joe \\")
 # print("репавн", 5//3)
 # print("репавн",min(5,6,8,9),round(5/5))
-
-input("напиши свой ник: ")
-
-
+number=4 #int
+print("sfgklj",number)
+number=8
+print("rezult",number)
+del(number)
+str_nuy ='67' #string
+goliu ="6.79060" #float
+world ="jaked:" #string
+goloub=True #bool
+# print(world,goloub)
+# print(world +str(goliu))
+number=8
+print(world+str(number+int(str_nuy)+bool(str_nuy)))
